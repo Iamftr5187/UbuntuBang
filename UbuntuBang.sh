291e330a213a9b74ab6835d6654f2f41f1944453
Iamftr5187 <ashleyf979@outlook.com>
2016-08-18 16:59:06 +0100

    Update UbuntuBang.sh

--- UbuntuBang.sh
+++ UbuntuBang.sh
@@ -17,16 +17,24 @@ apt-get update
 apt-get upgrade
 
 ###########################################################
+#Install english
+sudo apt-get install language-pack-en language-pack-en-base manpages
+
+###########################################################
 # Install apt
 apt-get -y install build-essential linux-headers-`uname -r` xorg openbox obmenu lxappearance xcompmgr nitrogen tint2 numlockx conky terminator leafpad pcmanfm lxtask gmrun gnome-screenshot firefox firefox-locale-ko flashplugin-nonfree libreoffice evince mirage inkscape gimp smplayer cups-pdf convertall qalculate gksu synaptic language-pack-ko fonts-noto-cjk fonts-nanum* uim uim-byeoru libappindicator3-1 libhangul1 libqt5core5a libqt5gui5 libqt5widgets5 libqtcore4 libqtgui4 qtbase-abi-5-2-1 im-config dconf-editor libreoffice evince mirage inkscape gimp smplayer cups-pdf convertall qalculate libqt4-core libxss1 libappindicator1 libindicator7 gksu synaptic numix-icon-theme-circle shimmer-themes
 
 ###########################################################
+#Install apt-fast
+sudo add-apt-repository ppa:saiarcot895/myppa
+sudo apt-get update
+sudo apt-get -y install apt-fast
+
+###########################################################
 # wget
 wget https://github.com/Thestars3/arkzip/releases/download/v2.4.4/arkzip_2.4.4_amd64.deb
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 
 dpkg -i arkzip_2.4.4_amd64.deb
-dpkg -i google-chrome-stable_current_amd64.deb
 
 rm *.deb
 
@@ -36,9 +44,8 @@ add-apt-repository ppa:dasom
 apt-get -y install dasom dasom-gtk dasom-qt dasom-jeongeum
 
 ###########################################################
-# Default Web browser
-rm /etc/alternatives/x-www-browser
-ln -s /usr/bin/firefox /etc/alternatives/x-www-browser
+# Install chromium-browser
+sudo apt-get install chromium-browser
 
 ###########################################################
 # Conky
